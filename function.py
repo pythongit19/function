3713a04343a1ef0747a134999c10cfa59ebb8a80
shima98 <pytorch.python19@gmail.com>
2022-07-30 00:29:12 +0430

    add function

---
+++ function.py
@@ -0,0 +1,11 @@
+def hello_world():
+    print("hello world!")
+hello_world()
+
+def two_times(x):
+    return 2*x
+print(two_times(4))
+
+def my_function(first_name, last_name):
+    print(first_name+ " "+ last_name)
+my_function("john", "smith")
